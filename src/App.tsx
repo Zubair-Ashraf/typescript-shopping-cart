4062ef2edea414543c925239662220eae46740fd
Zubair-Ashraf <zubair8767844@gmail.com>
2021-09-14 15:20:27 +0500

    startup boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export interface IAppProps {
+}
+
+const App = (props: IAppProps) => {
+  return (
+    <div>
+      Hello Typo
+    </div>
+  );
+}
+
+export default App;
